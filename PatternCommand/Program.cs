94e60deac42ace7ccc19e5025d5629558cf3da81
Serejka <laktes1@gmail.com>
2020-09-28 11:56:21 +0500

    finish?

--- PatternCommand/Program.cs
+++ PatternCommand/Program.cs
@@ -24,6 +24,14 @@ namespace PatternCommand
                 new MinDate(new DateTime(2015, 7, 20), new DateTime(2016, 7, 20)),
                 new PlusMonth( new DateTime(2015, 12, 20)),
                 new SecToDate( 350000),
+                new MaxDate(new DateTime(2010, 7, 20), new DateTime(2020, 7, 20)),
+                new MinDate(new DateTime(2020, 7, 20), new DateTime(2010, 7, 20)),
+                new PlusMonth( new DateTime(2015, 01, 20)),
+                new SecToDate( 2365125),
+                new MaxDate(new DateTime(2000, 7, 20), new DateTime(2001, 7, 20)),
+                new MinDate(new DateTime(2001, 7, 20), new DateTime(2000, 7, 20)),
+                new PlusMonth( new DateTime(2015, 05, 20)),
+                new SecToDate( 9874562),
             };
                                
         }
